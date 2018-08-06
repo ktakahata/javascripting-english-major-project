4207264e709dc8ad81fc0de7dbcea65ceb2cbf09
Kimberly Takahata <kat2163@columbia.edu>
2018-08-06 12:43:08 -0400

    Created spiciness calculator

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
-let tipAmount;
-let tipCalculator = function(total, tipRate) {
-  let parsedTipRate;
-  if ( tipRate < 1 ) {
-    parsedTipRate = tipRate;
-  } else {
-    parsedTipRate = tipRate / 100;
+let myHabaneroSauceSquirts, myBurritoObject;
+myHabaneroSauceSquirts = 3;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: myHabaneroSauceSquirts,
+  spiciness: function(){
+    if (myHabaneroSauceSquirts > 0 ){
+      alert("This is a spicy burrito!");
+    } else {
+      alert("This is a mild burrito.");
+    }
   }
-  $("#response").html("Your tip is $" + (parsedTipRate * total));
 };
- tipCalculator(50.00, 30);
-console.log(tipAmount);
+$("#response").html("Your burrito has " +
+  myBurritoObject.habaneroSauceSquirts +
+  "squirts of habanero.");
+myBurritoObject.spiciness();
