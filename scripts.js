7121f1deee67683356078e93dd56c3a7d3fd79b2
Kimberly Takahata <kat2163@columbia.edu>
2018-08-05 21:26:35 -0400

    Fixed odd/even function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-let tipCalculator;
-tipCalculator = function(total, tipRate){
-  let tipAmount;
-  tipAmount = tipRate * total;
-  $("#response").html("Your tip is $" + tipAmount);
-};
-
-tipCalculator(50.00, 0.2);
+let userInput;
+userInput = prompt ("Please input a number", "Type a number here.");
+for (let i = 1; i <= userInput; i = i + 1 ) {
+  if ( i % 2 === 0 ) {
+    $("#response").append("<br />" + i + " is even");
+  } else {
+    $("#response").append("<br />" + i + " is odd");
+  }
+}
