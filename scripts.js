9cef4674402830aa1f6c3f1b9e3c2a7a6ba6e401
Kimberly Takahata <ktakahata5@gmail.com>
2019-03-07 09:55:37 -0500

    added scripts.js alert

--- scripts.js
+++ scripts.js
@@ -1,18 +1,1 @@
-let userString, upperCaseMinusE, upperCasedString;
-userString = prompt("What do you want to UPPeRCASe?");
-upperCaseMinusE = function(string){
-  let result;
-  result = "";
-  for (let i = 0; i < string.length ; i = i + 1) {
-    let letter;
-    letter = string[i];
-    if ( letter === "e" ) {
-      result = result + letter;
-    } else {
-      result = result + letter.toUpperCase();
-    }
-  }
-  return result;
-};
-upperCasedString = upperCaseMinusE(userString);
-$("#response").html(upperCasedString);
+alert("scripts.js has loaded!");
