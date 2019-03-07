f417ecff56f882d9de4b9b86f0a98e587115945d
Kimberly Takahata <ktakahata5@gmail.com>
2019-03-07 10:23:24 -0500

    writing loop program

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-let userInput;
-userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
-if ( userInput === "burrito" ) {
-  $("#response").html("Brilliant choice!");
-} else {
-  $("#response").html("Don’t you want a burrito?");
+let i;
+i = 1;
+while ( i < 4 ) {
+  $("#response").append("<br />" + i);
+  i = i + 1;
 }
