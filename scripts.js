4146c7a97f50967d006667cf4906defcd6e12b85
Kimberly Takahata <ktakahata5@gmail.com>
2019-03-07 10:38:06 -0500

    "added prompt for number; display all numbers up to input"

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
-for (let i = 1; i < 4; i = i + 1 ) {
+let userInput;
+userInput = prompt("Please enter a number");
+for (let i = 1; i<= userInput; i = i + 1 ) {
   $("#response").append("<br />" + i);
 }
